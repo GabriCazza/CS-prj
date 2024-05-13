2589d01fd99b3cc26f281222d9acf1f3c268f5fd
GabriCazza <165047806+GabriCazza@users.noreply.github.com>
2024-05-13 15:41:36 +0200

    a

--- clock.py
+++ clock.py
@@ -1,17 +1,12 @@
 import streamlit as st
 
-def clock_main():
-    
-    
-    # Embed a publicly available JavaScript clock
-    # This example uses a generic clock URL which you should replace with the actual URL of the clock you want to embed
+def main():
+    st.title('Stylish Clock')
+    # Replace 'your_clock_url.html' with the URL where your clock is hosted
     clock_html = """
-    <iframe src='https://free.timeanddate.com/clock/i8ifsfw7/n268/szw160/szh160/hbw0/hbh0/hbc000/cf100/hgr0/fav0/fiv0/mqcfff/mqs2/mql3/mqw1/mqd78/mhcfff/mhs2/mhl3/mhw1/mhd78/mmv0/hwm2/hhcbbb/hhs2/hmcbbb/hms2/hscbbb' frameborder='0' width='160' height='160'></iframe>
+    <iframe src='clock.html' width='210' height='210' frameborder='0'></iframe>
     """
-    
-    # Display the HTML iframe in Streamlit
     st.markdown(clock_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
-    clock_main()
-
+    main()
